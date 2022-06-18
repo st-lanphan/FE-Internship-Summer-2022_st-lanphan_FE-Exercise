df01c163b6638590746b780a4be04b3ad071879f
lan phan <lanphan@supremetech.vn>
2022-06-18 15:17:35 +0700

    create list and list product UI

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,73 +1,82 @@
-const productList = document.querySelectorAll(".list-products");
-console.log(productList);
-eventListeners();
-function eventListeners() {
-  window.addEventListener("DOMContentLoaded", () => {
-    renderData();
-  });
+function initData() {
+  const data = [
+    {
+      name: 'T-Shirt Summer Vibes',
+      image_url: './image/product-1.png',
+      salePrice: 119.99,
+      price: 130.25,
+      discount: 30,
+    },
+    {
+      name: 'Loose Knit 3/4 Sleeve',
+      image_url: './image/product-2.png',
+      price: 119.99,
+      discount: 0,
+    },
+    {
+      name: 'Basic Slim Fit T-Shirt',
+      image_url: './image/product-3.png',
+      price: 79.99,
+      discount: 0,
+    },
+    {
+      name: 'Loose Textured T-Shirt',
+      image_url: './image/product-4.png',
+      price: 119.99,
+      discount: 0,
+    },
+  ];
+  window.localStorage.setItem("products", JSON.stringify(data));
+}
+
+function getData() {
+  const products = window.localStorage.getItem("products");
+  return JSON.parse(products);
 }
 
 function renderData() {
-  fetch("./javaScript/data.json", {
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      let html = "";
-      data.forEach((product) => {
-        console.log(product.discount);
-        if (product.discount <= 0) {
-          html += `  <li class="col-3 col-sm-6 foryou-item">
-        <div class="card">
+  // products = [{ name: '', ...}]
+  const products = getData();
+
+  // productListElements = [{...}]
+  const productListElements = document.querySelectorAll(".list-products");
+
+  const productElements = products.map((product) => {
+    let discountELement = " ";
+    if (product.discount > 0) {
+      discountELement = `
+      <span class="btn badge badge-product">-${product.discount}%</span>
+        <span class="description-product description-sale">
+          ${product.salePrice}
+        </span>
+      `;
+    }
+    const productElement = `<li class="col-3 col-sm-6">
+        <div class="card ">
           <div class="card-image">
-            <img
-              class="image-product"
-              src="${product.image_url}"
-              alt="Imgae-product"
-            />
+            <img class="image-product" src="${product.image_url}" alt="Imgae-product"/>
           </div>
           <div class="card-content">
             <a href="#" class="card-title product">
               <h4 class="title-product">${product.name}</h4>
             </a>
             <div class="price">
+              ${discountELement}
               <span class="card-price">${product.price}</span>
             </div>
           </div>
         </div>
-      </li>`;
-        } else {
-          html += `  <li class="col-3 col-sm-6 foryou-item">
-          <div class="card">
-            <div class="card-image">
-              <img
-                class="image-product"
-                src="${product.image_url}"
-                alt="Imgae-product"
-              />
-            </div>
-            <div class="card-content">
-              <span class="btn badge badge-product">${product.discount}</span>
-              <a href="#" class="card-title product">
-                <h4 class="title-product">${product.name}</h4>
-              </a>
-              <div class="price">
-                <span class="description-product description-sale">
-                  ${product.salePrice}
-                </span>
-                <span class="card-price">${product.price}</span>
-              </div>
-            </div>
-          </div>
-        </li>`;
-        }
-      });
-      productList.forEach((product) => {
-        product.innerHTML += html;
-      }) 
+      </li>`
+    return productElement;
+  });
+
+  productListElements.forEach((element) => {
+    productElements.forEach((productElement) => {
+      element.innerHTML += productElement;
     });
+  });
 }
+
+initData();
+
+renderData();
