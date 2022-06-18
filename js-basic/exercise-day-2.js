f4745b63ff57d9fa3cb5efa02b679d2945b98449
lan phan <lanphan@supremetech.vn>
2022-06-18 08:32:30 +0700

    exercise day 2 js file

--- js-basic/exercise-day-2.js
+++ js-basic/exercise-day-2.js
@@ -4,17 +4,17 @@ const tabsContent = document.querySelectorAll(".tab-content");
 for(let i = 0; i <= tabs.length; i++) {
   tabs[i].addEventListener("click", function (e) {
     for(let j = 0; j < tabs.length; j++) {
-      if(tabs[j].classList.contains('current')) {
-        tabs[j].classList.remove('current');
+      if(tabs[j].classList.contains("current")) {
+        tabs[j].classList.remove("current");
       }
     }
-    tabs[i].classList.add('current');
+    tabs[i].classList.add("current");
     let contents = document.getElementById(tabs[i].dataset.tab);
     for(let j = 0; j < tabsContent.length; j++) {
-      if(tabsContent[j].classList.contains('current')) {
-        tabsContent[j].classList.remove('current');
+      if(tabsContent[j].classList.contains("current")) {
+        tabsContent[j].classList.remove("current");
       }
     }
-    contents.classList.add('current')
+    contents.classList.add("current")
   });
 }
