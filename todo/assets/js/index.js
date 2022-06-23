167fc2fec3baa54962596f447278567b33446265
lan phan <lanphan@supremetech.vn>
2022-06-23 20:35:55 +0700

    todo list

--- todo/assets/js/index.js
+++ todo/assets/js/index.js
@@ -1,59 +1,79 @@
-var TodoElements = document.querySelector(".list-todo");
-var listTodo = [];
+var listKeys = {
+  todoList: 'todo'
+}
+
+function setStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+function getStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+var todoElements = document.querySelector('.js-list-todo');
 
 function randomId() {
   var id = Math.random();
   return id;
 }
 function addTodo() {
-  var getContent = document.getElementById("myInput").value;
-  console.log(getContent);
-  var getStrongeItem = localStorage.getItem("todo");
-  var listTodo = JSON.parse(getStrongeItem) || [];
-  if (listTodo) {
-    listTodo.push ({
-        id: randomId(),
-        text: getContent
-      });
-  };
-  localStorage.setItem('todo', JSON.stringify(listTodo));
-  renderListTodo();
-}
-function renderListTodo () {
-  var todo = JSON.parse(localStorage.getItem("todo"));
-  console.log(typeof(todo));
-  if(todo) {
-    todo.forEach(function(element) {
-      htmlTodo =
-        '<li class="js-list-item"><spans class="js-content">' +
-        element.text +
-        '</spans> <button type="button" class="btn js-btn-delete">X</button></li>';
-      TodoElements.innerHTML += htmlTodo;
+  var getContent = document.getElementById('js-input').value;
+  var todo = getStorage(listKeys.todoList) || [];
+  // var listTodo = JSON.parse(getStrongeItem) || [];
+  if (todo) {
+    todo.push({
+      id: randomId(),
+      text: getContent,
     });
   }
+  setStorage(listKeys.todoList,todo);
+  renderListTodo();
+}
+function renderListTodo() {
+  var todo = getStorage(listKeys.todoList) || {};
+  if (todo) {
+    var htmlTodo = "";
+    todo.forEach(function (element) {
+      htmlTodo +=
+        "<li id=" +
+        element.id +
+        ' class="js-list-item">' +
+        '<spans class="js-content">' +
+        element.text +
+        '</spans><button type="button" js-data-id=' +
+        element.id +
+        ' class="btn js-btn-remove">Delete</button></li>';
+    });
+    todoElements.innerHTML = htmlTodo;
+  }
+  handleListenerRemove();
+}
+function handleListenerRemove() {
+  var htmlTodoItemRemove = document.getElementsByClassName('js-btn-remove');
+  var buttonRemove = Object.values(htmlTodoItemRemove);
+  buttonRemove.forEach(function (button) {
+    var todoId = button.getAttribute('js-data-id');
+    button.addEventListener('click', function (e) {
+      removeTodo(todoId);
+    });
+  });
 }
 function removeTodo(id) {
-  var todo = JSON.parse(localStorage.getItem("todo"));
+  var todo = getStorage(listKeys.todoList)|| {};
   var result = todo.filter(function (element) {
     return +element.id !== +id;
   });
-  console.log(result);
-  // localStorage.setItem("todo",result);
-  // var removeElement = document.getElementById(id);
-  // if (removeElement) {
-  //   removeElement.remove();
-  // }
-  // todo.forEach(function(element) {
-  //   if(element === id) {
-  //     todo.splic
-  //   }
-  // })
+  setStorage(listKeys.todoList,result);
+  var removeElement = document.getElementById(id);
+  if (removeElement) {
+    removeElement.remove();
+  }
+  renderListTodo();
 }
 
-
-var buttonAdd = document.querySelector(".btn-add-todo");
-buttonAdd.addEventListener("click", function (e) {
+var buttonAdd = document.querySelector('.js-btn-add-todo');
+buttonAdd.addEventListener('click', function (e) {
   addTodo();
 });
 renderListTodo();
-removeTodo();
+handleListenerRemove();
